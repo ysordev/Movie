3c5d94d94758e3832c25dd73971fdbce4c77c2d1
Rose <marieroseramire11@gmail.com>
2025-06-15 14:03:14 +0800

    initaial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "dd2ec1bd23aef6eeaf1698dd25d30e4c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
